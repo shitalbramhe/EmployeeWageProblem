252293c73474c3df83c53dabd6690070d5ec30b5
shital bramhe <shitalbramhe@gmail.com>
2022-01-07 14:23:06 +0530

    [Shital] add : check employee is present or absent

---
+++ EmployeeWageProblem.js
@@ -0,0 +1,12 @@
+//UC1
+
+const IS_ABSENT=0;
+let empcheck = Math.floor(Math.random()*10)%2;
+if(empcheck==IS_ABSENT)
+{
+    console.log("Employee is absent");
+}
+else
+{
+console.log("Employee is present");
+}
