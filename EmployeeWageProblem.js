47e3823901be13cc51c258c7ac84398eea15c410
shital bramhe <shitalbramhe@gmail.com>
2022-01-07 14:49:58 +0530

    [Shital] add : Calculate monthly working hours

--- EmployeeWageProblem.js
+++ EmployeeWageProblem.js
@@ -41,7 +41,7 @@ function getWorkingHours(empCheck)
           return 0;
   }
 }
-
+/*
 {
       let empHrs=0;
       let empCheck=Math.floor(Math.random()*10)%3;
@@ -49,3 +49,15 @@ function getWorkingHours(empCheck)
       let empWage=empHrs*WAGE_PER_HOUR;
       console.log("UC3 - Hour: "+empHrs+" Emp Wage: "+empWage);
 }
+*/
+//UC 4
+ 
+ let totalEmpHrs=0;
+ for(let day=0;day<NUM_OF_WORKING_DAYS;day++)
+ {
+   let empCheck=Math.floor(Math.random()*10)%3;
+   totalEmpHrs+=getWorkingHours(empCheck);
+ }
+
+ let empWage=totalEmpHrs*WAGE_PER_HOUR;
+ console.log("Hour: "+totalEmpHrs+" Emp Wage: "+empWage);
